067f0f1c44866b2bed46fea1654116647d2bd163
Amal Jose <amaljose96@gmail.com>
2024-03-25 22:20:32 -0700

    Added more script

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
+sudo apt-get update
+sudo apt install python3-pip -y
+sudo pip3 install virtualenv 
+
+alias python="python3"
 #Create environment
- python -m venv env
- source env/bin/activate
+python -m venv env
+source env/bin/activate
 # Install requirements
 pip install -r requirements.txt
 # start server

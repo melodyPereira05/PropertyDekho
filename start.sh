061ff96298f68fd51188a3e5c907cbdaf077668e
Amal Jose <amaljose96@gmail.com>
2024-03-25 22:24:14 -0700

    Amal (#3)
    * Added deploy script
    
    * Added env to environment
    
    * Added more script
    
    * Added port number

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
+
+sudo apt-get update
+sudo apt install python3-pip -y
+sudo pip3 install virtualenv 
+
+alias python="python3"
 #Create environment
- python -m venv env
- source env/bin/activate
+python -m venv env
+source env/bin/activate
 # Install requirements
 pip install -r requirements.txt
 # start server
-python manage.py runserver
+python manage.py runserver 8080
